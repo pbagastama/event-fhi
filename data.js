bcaccacd0f5fbedfa5bb97e7311129dfeb7fdbec
Bagas <bagas@microad.co.id>
2021-04-20 14:25:49 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,66 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-main-event",
+      "name": "Main Event",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "yaw": -0.05588952085298082,
+        "pitch": 0.20908102775576332,
+        "fov": 1.3460094916088818
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9401012130247857,
+          "pitch": -0.3643518182788341,
+          "rotation": 9.42477796076938,
+          "target": "0-main-event"
+        },
+        {
+          "yaw": -2.663419165101807,
+          "pitch": -0.30723549763261104,
+          "rotation": 3.141592653589793,
+          "target": "0-main-event"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2845125461071536,
+          "pitch": -0.06218002163405245,
+          "title": "Main Room",
+          "text": "Tempat acara berlangsung"
+        },
+        {
+          "yaw": 2.951262514722923,
+          "pitch": -0.03225903332223723,
+          "title": "Room Meeting Zoom&nbsp;",
+          "text": "Masuk ke event zoom"
+        },
+        {
+          "yaw": -2.6514330598172897,
+          "pitch": 0.0002614261681994634,
+          "title": "Room Game",
+          "text": "Acara penutup"
+        }
+      ]
+    }
+  ],
+  "name": "FHI-Events",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
